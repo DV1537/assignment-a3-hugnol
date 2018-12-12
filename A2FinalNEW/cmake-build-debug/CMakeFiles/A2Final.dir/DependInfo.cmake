8496a22479e79f6cdda20cdc82467a3bcc666c24
hugnol <44838038+hugnol@users.noreply.github.com>
2018-12-12 15:14:54 +0100

    Add files via upload

---
+++ A2FinalNEW/cmake-build-debug/CMakeFiles/A2Final.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/hugon/Desktop/A2FinalNEW/FileFunctions.cpp" "C:/Users/hugon/Desktop/A2FinalNEW/cmake-build-debug/CMakeFiles/A2Final.dir/FileFunctions.cpp.obj"
+  "C:/Users/hugon/Desktop/A2FinalNEW/Line.cpp" "C:/Users/hugon/Desktop/A2FinalNEW/cmake-build-debug/CMakeFiles/A2Final.dir/Line.cpp.obj"
+  "C:/Users/hugon/Desktop/A2FinalNEW/Point.cpp" "C:/Users/hugon/Desktop/A2FinalNEW/cmake-build-debug/CMakeFiles/A2Final.dir/Point.cpp.obj"
+  "C:/Users/hugon/Desktop/A2FinalNEW/Polygon.cpp" "C:/Users/hugon/Desktop/A2FinalNEW/cmake-build-debug/CMakeFiles/A2Final.dir/Polygon.cpp.obj"
+  "C:/Users/hugon/Desktop/A2FinalNEW/Triangle.cpp" "C:/Users/hugon/Desktop/A2FinalNEW/cmake-build-debug/CMakeFiles/A2Final.dir/Triangle.cpp.obj"
+  "C:/Users/hugon/Desktop/A2FinalNEW/main.cpp" "C:/Users/hugon/Desktop/A2FinalNEW/cmake-build-debug/CMakeFiles/A2Final.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
